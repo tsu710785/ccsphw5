e125b416428d73b2bea2da43eb0f219a79d71469
imet <imet@imet-Ubuntu.(none)>
2014-06-02 20:18:32 +0800

    hw5

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -9,12 +8,12 @@ var path = require('path');
 var flash = require('connect-flash');
 var mongoose = require('mongoose');
 var passport = require('passport');
-
-// require('./config/db'); // TODO [DB] : Connect to database
-// require('./config/passport'); // TODO [FB] : Passport configuration
+var promise = require('promise');
+require('./config/db'); // TODO [DB] : Connect to database
+require('./config/passport'); // TODO [FB] : Passport configuration
 
 var app = express();
-// var Vote = mongoose.model('Vote'); // TODO [DB] : Get Vote model
+var Vote = mongoose.model('Vote'); // TODO [DB] : Get Vote model
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -52,28 +51,28 @@ app.post('/vote', function(req, res, next){
   // Stores the voted option (conveted to number) into session
   req.session.vote = +req.body.vote;
 
-  res.redirect('/result');
+  //res.redirect('/result');
 
   /* TODO [FB] : Redirect to passport auth url! */
   // Directly invoke the passport authenticate middleware.
   // Ref: http://passportjs.org/guide/authenticate/
   //
-  // passport.authenticate('facebook')(req, res, next);
+  passport.authenticate('facebook')(req, res, next);
 });
 
 // TODO [FB]: Facebook callback handler
 // Ref: https://github.com/jaredhanson/passport-facebook/blob/master/examples/login/app.js#L100
 //
-// app.get('/fbcb', passport.authenticate('facebook', {
-//   successRedirect:'/result',
-//   failureRedirect: '/'
-// }));
+app.get('/fbcb', passport.authenticate('facebook', {
+  successRedirect:'/result',
+  failureRedirect: '/'
+}));
 
 app.get('/result', function(req, res){
 
   var vote = req.session.vote, // The voted item (0~6)
-      fbid = "" + Math.random();    // Facebook ID. (Fake)
-      // fbid = req.user && req.user.id; // TODO [FB]: Get user from req.user
+      //fbid = "" + Math.random();    // Facebook ID. (Fake)
+      fbid = req.user && req.user.id; // TODO [FB]: Get user from req.user
 
   // Delete the stored session.
   //
@@ -94,20 +93,41 @@ app.get('/result', function(req, res){
   */
 
   //
-  // var vote = new Vote({vote: vote, fbid: fbid});
-  // vote.save(function(err, newVote){
-  //   if( err ){
-  //     req.flash('info', "你已經投過票囉！");
-  //     return res.redirect('/');
-  //   }
+  var vote = new Vote({vote: vote, fbid: fbid});
+  vote.save(function(err, newVote){
+    if( err ){
+      req.flash('info', "你已經投過票囉！");
+      return res.redirect('/');
+    }
+    var p = [];
+    var voting = [];
+    var sum = 0;
+    Vote.aggregate(
+      { $group: { _id: "$vote", num: {$sum : 1}}}
+      , function (err, result) {
+        if (err) console.log(err);
+        console.log(result); // [ { maxBalance: 98000 } ]
+        promise.all(result).then(function(values){
+          for(var i = 0, len= values.length; i < len; i++){
+            voting[values[i]._id] = values[i].num;
+            sum += values[i].num;
+          }
+          for(var j = 0; j < 7; j++){
+            if (voting[j]==null){
+              p[j] = 0;
+            }
+            else{
+              p[j] = voting[j]/sum * 100;
+            }
+            console.log("p: " + p[j]);
+          }
+          res.render('result', {
+            votes: [p[0], p[1], p[2], p[3], p[4], p[5], p[6]]// Percentages
+          });
+        });
+    });
   //
-  //   ... ...
-  //
-       res.render('result', {
-         votes: [18.1, 12.5, 42.44445, 21.3, 1.3, 2.5, 1.85555] // Percentages
-       });
-  //
-  // });
+  });
 
 });
 
